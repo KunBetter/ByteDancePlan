4d194f40f4573e48a8eaa74352d27f9dea273ab3
李信息 <xinxi.li@dianping.com>
2019-06-04 10:45:33 +0800

    google translate

---
+++ translate.py
@@ -0,0 +1,13 @@
+from googletrans import Translator
+
+translator = Translator()
+
+
+def googleTranslate(english):
+    return translator.translate(english, dest='zh-cn').text
+
+
+if __name__ == '__main__':
+    english = 'hello'
+    chinese = googleTranslate(english)
+    print(chinese)
