efaad503112bdb89b3fa264774488658587c1983
李信息 <xinxi.li@dianping.com>
2019-06-12 11:12:49 +0800

    url uniq

--- crawl.py
+++ crawl.py
@@ -9,17 +9,16 @@ def crawl_refinery29(url):
     response = requests.get(url)
     page_soup = bs4.BeautifulSoup(response.text, "lxml")
     # Latest News
-    latest_news = []
+    urls = []
     news_lists = page_soup.findAll("div", {"class": "slick-track"})
     news_lists_soup = bs4.BeautifulSoup(str(news_lists[0]), 'lxml')
     print('--------------Fetch Latest News----------')
     for content in news_lists_soup.div.contents:
         content_soup = bs4.BeautifulSoup(str(content), 'lxml')
-        latest_news.append(url + str(content_soup.div.a['href']))
-    print('Latest News Count : ' + str(len(latest_news)))
+        urls.append(url + str(content_soup.div.a['href']))
+    print('Latest News Count : ' + str(len(urls)))
     # trending and now on R29
     print('--------------Fetch Trending and Now on R29----------')
-    trending_and_now_on_r29 = []
     modules = page_soup.findAll("div", {"class": "module"})
     for module in modules:
         module_soup = bs4.BeautifulSoup(str(module), 'lxml')
@@ -39,7 +38,7 @@ def crawl_refinery29(url):
                     cards = row_soup.findAll("div", {"class": "card"})
                     for card in cards:
                         card_soup = bs4.BeautifulSoup(str(card), "lxml")
-                        trending_and_now_on_r29.append(url + str(card_soup.div.a['href']))
+                        urls.append(url + str(card_soup.div.a['href']))
                         '''
                         # section
                         card_section = card_soup.findAll("div", {"class": "section"})
@@ -50,16 +49,16 @@ def crawl_refinery29(url):
                         card_title_soup = bs4.BeautifulSoup(str(card_title), "lxml")
                         print(str(card_title_soup.span.text))
                         '''
-    print('Trending and Now on R29 Count : ' + str(len(trending_and_now_on_r29)))
+    print('Trending and Now on R29 Count : ' + str(len(urls)))
     fn = open("/Users/anthony/Desktop/R29.txt", "w")
-    print('--------------Translating Latest News----------')
+    print('--------------Translating----------')
     progress_rate = 0
-    latest_news_2 = list(set(latest_news))
-    for resource in latest_news_2:
+    urls_2 = list(set(urls))
+    for resource in urls_2:
         print('Translating: ' + resource)
         url, title, cn_title, text, cn_text, cn_text_len = crawl_refinery29_detail_page(resource)
         progress_rate = progress_rate + 1
-        if 0 < cn_text_len <= 1500:
+        if 0 < cn_text_len <= 2000:
             fn.write('=====================================================================================\n')
             fn.write('【原文链接】 ' + url + '\n')
             fn.write('【原文标题】 ' + title + '\n')
@@ -70,26 +69,7 @@ def crawl_refinery29(url):
         if progress_rate % 5 == 0:
             print('Has Translate ' + str(progress_rate) + ' pages ...')
         time.sleep(2)
-    print('--------------Latest News Translate Done!----------')
-    print('--------------Translating Trending and Now on R29----------')
-    progress_rate = 0
-    trending_and_now_on_r29_2 = list(set(trending_and_now_on_r29))
-    for resource in trending_and_now_on_r29_2:
-        print('Translating: ' + resource)
-        url, title, cn_title, text, cn_text, cn_text_len = crawl_refinery29_detail_page(resource)
-        progress_rate = progress_rate + 1
-        if 0 < cn_text_len <= 1500:
-            fn.write('=====================================================================================\n')
-            fn.write('【原文链接】 ' + url + '\n')
-            fn.write('【原文标题】 ' + title + '\n')
-            fn.write('【谷歌翻译标题】 ' + cn_title + '\n')
-            fn.write('【谷歌翻译正文字数】 ' + str(cn_text_len) + '\n')
-            fn.write('【原文正文】\n' + text)
-            fn.write('【谷歌翻译正文】\n' + cn_text + '\n')
-        if progress_rate % 5 == 0:
-            print('Has Translate ' + str(progress_rate) + ' pages ...')
-        time.sleep(2)
-    print('--------------Trending and Now on R29 Translate Done!----------')
+    print('--------------Translate Done!----------')
     fn.close()
 
 
@@ -140,5 +120,5 @@ def crawl_refinery29_detail_page(url, debug=False):
 if __name__ == '__main__':
     crawl_refinery29('https://www.refinery29.com')
 
-    # refinery29_one_page_url = 'https://www.refinery29.com/en-us/2019/06/234807/unc-charlotte-shooting-survivor-mother-daughter-gun-violence'
+    # refinery29_one_page_url = 'https://www.refinery29.com/en-us/2019/06/234852/how-to-watch-womens-world-cup-2019-live-stream'
     # crawl_refinery29_detail_page(refinery29_one_page_url, True)
